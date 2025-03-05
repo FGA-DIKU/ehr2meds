8146fd820f3628d28a0e589f492288b3644b3e28
Kiril Klein <kikl@di.ku.dk>
2025-03-05 21:35:52 +0100

    black

--- MEDS_preprocess/run_remote.py
+++ MEDS_preprocess/run_remote.py
@@ -8,5 +8,7 @@ job_name = sys.argv[1]
 env = Environment.get(ws, "MEDS", version="10")
 
 ct = "CPU-20-LP"
-src = ScriptRunConfig(source_directory=".", script=job_name+".py", compute_target=ct, environment=env)
+src = ScriptRunConfig(
+    source_directory=".", script=job_name + ".py", compute_target=ct, environment=env
+)
 Experiment(ws, name="MEDS").submit(src)
