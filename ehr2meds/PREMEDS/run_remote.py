0b77616de22845f6d2bc6c8d974e59d747e905f2
Kiril Klein <kikl@di.ku.dk>
2025-03-07 17:49:13 +0100

    update run remote to pass config arg

--- ehr2meds/PREMEDS/run_remote.py
+++ ehr2meds/PREMEDS/run_remote.py
@@ -3,12 +3,18 @@ from azureml.core import Workspace, Environment, Experiment, ScriptRunConfig
 
 ws = Workspace.from_config()
 
+# The first argument is the job name; any additional arguments will be passed to the script
 job_name = sys.argv[1]
+job_args = sys.argv[2:]
 
 env = Environment.get(ws, "MEDS", version="10")
 
 ct = "CPU-20-LP"
 src = ScriptRunConfig(
-    source_directory=".", script=job_name + ".py", compute_target=ct, environment=env
+    source_directory=".",
+    script=f"{job_name}.py",
+    compute_target=ct,
+    environment=env,
+    arguments=job_args,  # Pass additional arguments here
 )
 Experiment(ws, name="MEDS").submit(src)
