b27b6302bb53424ab78d36e7581d092d338427e0
Kiril Klein <kikl@di.ku.dk>
2025-05-16 11:34:16 +0200

    add extract script

---
+++ ehr2meds/PREMEDS/extract_lab_dist.py
@@ -0,0 +1,56 @@
+import gzip
+import pathlib
+import pickle
+import shutil
+from os.path import dirname, join
+
+from ehr2meds.PREMEDS.preprocessing.extraction.extractor import ValueExtractor
+from ehr2meds.PREMEDS.preprocessing.io.config import load_config
+from ehr2meds.PREMEDS.preprocessing.io.logging import setup_logging
+
+
+def my_app(config_path):
+    """
+    Run normalization with the given config file.
+
+    :param config_path: Full path to the config file
+    """
+    # Load config directly from provided path
+    cfg = load_config(config_path)
+
+    # Create output directory (parent directory of the output file)
+    output_dir = dirname(cfg.paths.output_dir)
+    pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
+
+    # Copy config to output directory
+    shutil.copyfile(
+        config_path,
+        join(output_dir, "config.yaml"),
+    )
+
+    # Setup logging
+    setup_logging(
+        log_dir=cfg.get("logging", {}).get("path"),
+        log_level=cfg.get("logging", {}).get("level"),
+        name="normalise.log",
+    )
+
+    lab_val_dict = ValueExtractor(cfg)()
+
+    # Compress with gzip when saving
+    with gzip.open(join(output_dir, "lab_val_dict.pickle.gz"), "wb") as f:
+        pickle.dump(lab_val_dict, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Normalization script")
+    parser.add_argument(
+        "--config",
+        type=str,
+        required=True,
+        help="Path to configuration file (.yaml)",
+    )
+    args = parser.parse_args()
+    my_app(args.config)
