6772fe635c216fec4074a9b2a4b5a26071d9c7b5
Kiril Klein <kikl@di.ku.dk>
2025-07-06 10:36:38 +0200

    adjust config

--- ehr2meds/PREMEDS/preprocessing/io/config.py
+++ ehr2meds/PREMEDS/preprocessing/io/config.py
@@ -53,7 +53,7 @@ class AdmissionsConfig:
     timestamp_out_column: str = "timestamp_out"
     admission_event_type: str = ADMISSION_IND.lower()
     transfer_event_type: str = "flyt ind"
-    use_admission_move: bool = False
+    use_adm_move: bool = False
     save_adm_move: bool = False
     rename_columns: Dict[str, str] = None
     filename: str = "admissions"
