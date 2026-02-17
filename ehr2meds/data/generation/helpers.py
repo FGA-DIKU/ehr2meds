3eecdb22d3a848651eb370049c67bb4f007cfa40
Katja-Jagd <knjagd@hotmail.com>
2026-02-17 07:18:02 +0100

    added helper functions, pnr, alphanumeric string, and date remformatting

--- ehr2meds/data/generation/helpers.py
+++ ehr2meds/data/generation/helpers.py
@@ -24,11 +24,25 @@ def rand_int(min_val=0, max_val=100):
 def rand_float(min_val=0.0, max_val=100.0):
     return random.uniform(min_val, max_val)
 
+def rand_date(start=1970, end=2020, format=None):
+    """
+    Random date generator with optional formatting.
 
-def rand_date(start=1970, end=2020):
+    Examples:
+    format=None) -> date object
+    format="%Y-%m-%d" -> "1999-02-01"
+    format="%d/%m/%Y" -> "01/02/1999"
+    format="%d%b%Y" -> "01FEB1999"
+    format="%d%m%Y" -> "01021999"
+    """
     delta = date(end, 12, 31) - date(start, 1, 1)
     random_days = random.randint(0, delta.days)
-    return date(start, 1, 1) + timedelta(days=random_days)
+    d = date(start, 1, 1) + timedelta(days=random_days)
+
+    if format:
+        return d.strftime(format).upper()
+
+    return d
 
 
 def rand_datetime(start=1970, end=2020):
@@ -60,6 +74,21 @@ def rand_string(min_length=10, max_length=100, include_digits=True):
         )
 
 
+def rand_string(min_length=10, max_length=100, include_digits=True):
+    if include_digits:
+        return "".join(
+            random.choices(
+                string.ascii_letters + string.digits,
+                k=random.randint(min_length, max_length),
+            )
+        )
+    else:
+        return "".join(
+            random.choices(
+                string.ascii_letters, k=random.randint(min_length, max_length)
+            )
+        )
+
 def choice(options):
     return random.choice(options)
 
@@ -74,7 +103,6 @@ def greater_than_datetime(min_date, end=2020):
     delta = datetime(end, 12, 31) - min_date
     random_seconds = random.randint(0, int(delta.total_seconds()))
     return min_date + timedelta(seconds=random_seconds)
-
 
 # Specialized functions for DST dataset
 def honuge(start_year, end_year):
@@ -91,3 +119,12 @@ def mix_function(functions, probabilities):
     selected_idx = random.choices(range(len(functions)), weights=probabilities, k=1)[0]
     selected_func = functions[selected_idx]
     return selected_func["func"](**selected_func["args"])
+
+def rand_alnum(length=8):
+    """
+    Returns a random alphanumeric string of given length.
+    """
+    chars = string.ascii_letters + string.digits
+    return "".join(random.choice(chars) for _ in range(length))
+
+import random
