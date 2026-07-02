c877b2c940315a838244d74fae8a6ecb6d5b322e
LlambiasMBP <llambias@live.com>
2026-07-02 19:16:24 +0200

    add low_memory

--- ehr2meds/preMEDS/data_handler.py
+++ ehr2meds/preMEDS/data_handler.py
@@ -31,7 +31,7 @@ class DataHandler:
         if filename.endswith(".parquet"):
             return pd.read_parquet(filename, columns=list(cols)).rename(columns=rename)
         elif filename.endswith((".csv", ".asc")):
-            df = pd.read_csv(filename)
+            df = pd.read_csv(filename, low_memory=False)
             return df[list(cols)].rename(columns=rename)
         else:
             raise ValueError(f"Unsupported file type: {filename}")
@@ -43,7 +43,7 @@ class DataHandler:
             for batch in pf.iter_batches(columns=list(cols), batch_size=self.chunksize):
                 yield batch.to_pandas().rename(columns=rename)
         elif filename.endswith((".csv", ".asc")):
-            for chunk in pd.read_csv(filename, chunksize=self.chunksize):
+            for chunk in pd.read_csv(filename, chunksize=self.chunksize, low_memory=False):
                 yield chunk[list(cols)].rename(columns=rename)
         else:
             raise ValueError(f"Unsupported file type: {filename}")
