e2d1022d3f338449cdda40da15f9c8d069090e8a
Katja-Jagd <knjagd@hotmail.com>
2026-07-14 14:36:23 +0200

    Move timezone normalization to utils

--- ehr2meds/preMEDS/processors.py
+++ ehr2meds/preMEDS/processors.py
@@ -6,6 +6,7 @@ from ehr2meds.preMEDS.utils import (
     clean_data,
     compose_columns,
     map_pids_to_ints,
+    remove_timezones,
     validate_subject_id,
 )
 from pathlib import Path
@@ -25,15 +26,16 @@ class Processor:
         2. OPTIONAL: Apply columns map
         3. OPTIONAL: Compose new columns from existing ones
         4. OPTIONAL: Apply pid integer mapping
-        5. clean data
-        6. validate subject_id column
+        5. Remove timezone information from timezone-aware datetime columns
+        6. Clean data
+        7. Validate subject_id column
         """
         df = apply_value_map(df, table_config.get("value_map", {}))
         df = Processor._apply_mappings(df, table_config.get("mappings", []), data_handler)
         df = compose_columns(df, table_config.get("compose", {}))
         if subject_id_mapping is not None:
             df = map_pids_to_ints(df, subject_id_mapping)
-        df = Processor._remove_timezones(df)
+        df = remove_timezones(df)
         df = clean_data(df)
         validate_subject_id(df)
 
@@ -66,11 +68,3 @@ class Processor:
             filename = str(Path(__file__).parent.parent / "resources" / filename)  # TODO: Seems very hacky
 
         return data_handler.load(filename, cols=cols)
-
-    @staticmethod
-    def _remove_timezones(df: pd.DataFrame) -> pd.DataFrame:
-        """Convert timezone-aware datetime columns to timezone-naive UTC."""
-        for col in df.select_dtypes(include=["datetimetz"]).columns:
-            df[col] = df[col].dt.tz_convert("UTC").dt.tz_localize(None)
-
-        return df
